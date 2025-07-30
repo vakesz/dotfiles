4bb761dace7a585a5df3ccb9042d09e54818b453
Gabor Pinkova <vakesz@users.noreply.github.com>
2025-07-30 19:55:47 +0200

    Update Neovim and install script: add Trouble.nvim to packer configuration and switch npm tools to pnpm

--- .config/nvim/lua/vakesz/packer.lua
+++ .config/nvim/lua/vakesz/packer.lua
@@ -23,4 +23,17 @@ return require('packer').startup(function(use)
 	  run = ':TSUpdate'
   }
 
+  use({
+      "folke/trouble.nvim",
+      config = function()
+          require("trouble").setup {
+              icons = false,
+              -- your configuration comes here
+              -- or leave it empty to use the default settings
+              -- refer to the configuration section below
+          }
+      end
+  })
+
+
 end)
